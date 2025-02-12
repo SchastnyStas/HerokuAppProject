8457f85f0271400c6ad27a7654165b06c6ac7382
Stanislau Schastny <stasschastny@gmail.com>
2025-02-12 21:28:48 +0100

    Initial commit

---
+++ src/test/java/tests/HerokuAppTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HerokuAppTest {
+    @Test
+    public void herokuAppTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/inputs");
+        driver.manage().window().maximize();
+        ;
+        driver.manage().window().setSize(new Dimension(800, 600));
+        driver.manage().window().setPosition(new Point(100, 100));
+        Point position = driver.manage().window().getPosition();
+        int x = position.getX();
+        int y = position.getY();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+}
