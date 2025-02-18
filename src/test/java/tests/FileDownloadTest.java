901c4d9dbbc15d61f283b437c1dad05d30bbb0cf
Stanislau Schastny <stasschastny@gmail.com>
2025-02-18 15:51:33 +0100

    Add Lesson20

--- src/test/java/tests/FileDownloadTest.java
+++ src/test/java/tests/FileDownloadTest.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.util.HashMap;
 
 public class FileDownloadTest {
+
     @Test
     public void checkFileDownLoad() throws InterruptedException {
         HashMap<String, Object> chromePrefs = new HashMap<>();
